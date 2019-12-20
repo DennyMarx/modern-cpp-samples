c70bfa8986a2221a8e181cfb5b795e5dc01e4e71
Denny Marx <Denny.Marx@audi.de>
2019-12-20 14:49:18 +0100

    getting rid of static globals

--- ThreadsFuturePromise/test/testThreadFuturePromise.cpp
+++ ThreadsFuturePromise/test/testThreadFuturePromise.cpp
@@ -5,15 +5,16 @@
 
 #include "Sensor.h"
 
+// future for data communication on-between threads
 static std::future<SensorData> _sensorDataFuture;
-static volatile bool _dataAvailable = false;
-static volatile bool _done = false;
+
+// promise and future to signal in-between threads
+static std::promise<void> _donePromise;
+static std::future<void> _doneFuture(_donePromise.get_future());
 
 
 void sensorThreadFunc() 
 {
-	_done = false;
-
 	// sensor test
 	Sensor mySensor;
 	mySensor.start();
@@ -23,26 +24,38 @@ void sensorThreadFunc()
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 		std::future<SensorData> sensorDataFuture = mySensor.run();
 		_sensorDataFuture = std::move(sensorDataFuture);
-		_dataAvailable = true;
 	}
 	
-	_done = true;
+	// signal done
+	_donePromise.set_value();
 }
 
 
 void algoThreadFunc()
 {
-	// consume the sensor data
-	while (!_done)
+	std::future_status _done = _doneFuture.wait_for(std::chrono::milliseconds(1));
+
+	// consume the sensor data as long as _done was not signaled
+	while (_done != std::future_status::ready)
 	{
+		// poll for new sensor data every 500ms
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-		if (_dataAvailable)
+		bool isSensorDataFutureValid = _sensorDataFuture.valid();
+		
+		if (isSensorDataFutureValid)
 		{
 			std::cout << "Sensor Result: " << _sensorDataFuture.get().sensorValue << std::endl;
-			_dataAvailable = false;
 		}
+		else
+		{
+			std::cout << "Sensor data not ready" << std::endl;
+		}
+		
+		_done = _doneFuture.wait_for(std::chrono::milliseconds(1));
 	}
+
+	std::cout << "Sensor is done" << std::endl;
 }
 
 
