295dab76dad9ff6b5264056aecaeb67ed6370e4d
Denny <Denny.Marx@posteo.de>
2019-12-25 12:32:20 +0100

    GTest is active

--- ThreadsFuturePromise/test/testSensor.cpp
+++ ThreadsFuturePromise/test/testSensor.cpp
@@ -1,10 +1,31 @@
 // unit test for the sensor object
 
 #include "gtest/gtest.h"
-#include "sensor.h"
+#include "Sensor.h"
+
+
+class SensorTest : public ::testing::Test 
+{
+    protected:
+        // the test object
+        std::unique_ptr<Sensor> _pSensorTestObj = nullptr;
+
+    // setup for each test
+    void SetUp() override 
+    {
+        _pSensorTestObj = std::make_unique<Sensor>();
+    }
+
+    // tear down after each test
+    void TearDown() override 
+    {
+        _pSensorTestObj.release();
+    }
+
+};
 
  
-TEST(Sensor, testActivateSensor)
+TEST_F(SensorTest, testStartSensor)
 {
-    EXPECT_EQ(1,0);
+    EXPECT_NO_THROW(_pSensorTestObj->start());
 }
